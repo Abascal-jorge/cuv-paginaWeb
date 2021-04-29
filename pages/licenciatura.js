b890b8ef67d9bda5b43896dd753d908abb329b92
Abascal <jorgeabascal20@gmail.com>
2021-04-29 10:50:39 -0500

    Agregando licenciaturas imagenes tarjetas

--- pages/licenciatura.js
+++ pages/licenciatura.js
@@ -1,80 +1,8 @@
 import React, { useEffect, useContext} from 'react';
 import Layout from "../components/Layout/layout";
-import styled from "@emotion/styled";
 import Link from "next/link";
-import useDatosContext from "../context/useDatosContext";
 import UseDatosContext from '../context/useDatosContext';
-
-const ContenidoPrincipal = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    div{
-        flex: 1;
-        padding: 5px;
-        img{
-            width: 100%;
-            object-fit: cover;
-        }
-    }
-`;
-
-const CardSetion = styled.div`
-
-    h2{
-        margin-top: 40px;
-        padding-top: 10px;
-        border-top: 2px solid #eee;
-    }
-
-    .card-licenciaturas{
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-        justify-content: center;
-        align-items: center;
-        column-gap: 10px;
-    }
-
-    .card{
-        height: 450px;
-        border: 1px solid #eee;
-        border-top-left-radius: 10px;
-        border-top-right-radius: 10px;
-        .card-img{
-            img{
-                width: 100%;
-                height: 300px;
-                object-fit: cover;
-                border-top-left-radius: 10px;
-                border-top-right-radius: 10px;
-            }
-        }
-
-        .card-info{
-            padding: 10px;
-            text-align: center;
-            h3{
-               
-            }
-            a{
-                color: blue;
-                font-weight: 700;
-            }
-        }
-    }
-`;
-
-const InfoUniversidad = styled.div`
-    .pregunta{
-        padding: 10px 0;
-        color: blue;
-    }
-
-    p{
-        text-align: justify;
-    }
-`;
+import { ContenidoPrincipal, CardSetion, InfoUniversidad } from "../components/Section/estilosLicenciatura"; 
 
 const Licenciatura = () => {
 
@@ -159,7 +87,7 @@ const Licenciatura = () => {
                                                         <Link href="/">Leer mas...</Link>
                                                     </div>
                                                 </div>
-                                        ))
+                                        )) 
                                     }
                                 </div>
                             </CardSetion>
